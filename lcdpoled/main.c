8b2a6f9ef45ac097900fda65f88dfc2c5f4e7843
jim winstead <jimw@trainedmonkey.com>
2013-12-02 22:34:56 -0800

    Initial Commit

---
+++ lcdpoled/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  lcdpoled
+//
+//  Created by Jim Winstead on 12/2/13.
+//  Copyright (c) 2013 Jim Winstead. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
